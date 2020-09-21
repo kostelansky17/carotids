165690bcb0f7b2a8496fdf4ae624ad8f9eacf63d
Martin Kostelansky <kostelansky17@github.com>
2020-09-21 19:54:56 +0200

    fix dependencies

--- categorization_train.py
+++ categorization_train.py
@@ -7,19 +7,13 @@ from torch.optim import lr_scheduler, SGD
 from torchvision import transforms
 from torchvision import models, transforms
 
-from carotids.categorization.categorization_cnn import (
+from carotids.categorization.models import (
     create_small_cnn,
     create_vgg,
     create_resnet50,
 )
-from carotids.categorization.categorization_dataset import CategorizationDataset
-from carotids.categorization.categorization_linear import (
-    create_categorization_features,
-    train_classifier,
-    try_hyperparameters,
-)
-from carotids.metrics import accuracy_np, accuracy_dataset
-from carotids.preprocessing import normalize_data
+from carotids.categorization.dataset import CategorizationDataset
+from carotids.metrics import accuracy_dataset
 from carotids.train_model import train_model
 from carotids.utils import GaussianNoiseTransform
 
@@ -83,23 +77,6 @@ BIG_TRANSFORMATIONS_TEST = transforms.Compose(
         transforms.Normalize([0.1145, 0.1144, 0.1134], [0.1694, 0.1675, 0.1684]),
     ]
 )
-
-
-def logreg_categorization(train_img_dirs, test_img_dirs):
-    print("Logistic regression for categorization...")
-    X_train, y_train = create_categorization_features(train_img_dirs)
-    X_test, y_test = create_categorization_features(test_img_dirs)
-    X_train, X_test = normalize_data(X_train, X_test)
-
-    Cs = [{"C": c} for c in np.logspace(0.01, 1, 10, endpoint=True)]
-    test_accs, best_C = try_hyperparameters(X_train, y_train, Cs)
-
-    clf = train_classifier(X_train, y_train, best_C)
-    test_acc = accuracy_np(clf.predict(X_test), y_test)
-
-    print(f"Test accuracy: {test_acc}")
-
-    return clf
 
 
 def cnn_categorization(model, train_transformations, test_transformations):
