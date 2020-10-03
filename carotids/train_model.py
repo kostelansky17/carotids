66f577aaa61a0af9066228c0482a0b7e8eecb4cf
Martin Kostelansky <kostelansky17@github.com>
2020-10-03 17:01:36 +0200

    add val acc and val evaluation

--- carotids/train_model.py
+++ carotids/train_model.py
@@ -1,25 +1,25 @@
 from copy import deepcopy
 from typing import Union
 
-from torch import device, Optimizer
+from torch import device, Optimizer, set_grad_enabled
 from torch.nn import Module
 from torch.nn.modules.loss import _Loss
 from torch.optim.lr_scheduler import _LRScheduler
 from torch.utils.data import DataLoader
 from torch.utils.data.dataset import Dataset
 
-from carotids.metrics import accuracy_torch
-from carotids.utils import train_val_split
+from carotids.metrics import accuracy_torch, evaluate_model_categorization
+from carotids.utils import split_dataset_into_dataloaders
 
 
 def train_model(
     model: Module,
-    train_data: Dataset,
+    train_loader: DataLoader,
+    val_loader: DataLoader,
     loss: _Loss,
     optimizer: Optimizer,
     device: device,
     scheduler: Union[None, _LRScheduler] = None,
-    val_split: float = 0.2,
     num_epochs: int = 75,
 ) -> tuple:
     """Trains the model on the training data.
@@ -52,12 +52,12 @@ def train_model(
     losses = {"train": [], "val": []}
     accuracies = {"train": [], "val": []}
 
-    train_loader, train_size, val_loader, val_size = train_val_split(
-        train_data, val_split
-    )
+    train_size = len(train_loader.dataset)
+    val_size = len(val_loader.dataset)
 
-    best_model = copy.deepcopy(model.state_dict())
+    best_model = deepcopy(model.state_dict())
     best_loss = 10 ** 8
+    best_accuracy = 0.0
 
     for epoch in range(num_epochs):
         print(f"Epoch {epoch}/{num_epochs - 1}")
@@ -72,7 +72,7 @@ def train_model(
             labels = labels.to(device)
 
             optimizer.zero_grad()
-            with torch.set_grad_enabled(True):
+            with set_grad_enabled(True):
                 outputs = model(inputs)
 
                 l = loss(outputs, labels)
@@ -85,40 +85,28 @@ def train_model(
                 train_epoch_loss += l.item() * inputs.size(0)
                 train_epoch_acc += accuracy_torch(outputs, labels) * inputs.size(0)
 
-        val_epoch_loss = 0.0
-        val_epoch_acc = 0
-        for inputs, labels in val_loader:
-            model.eval()
-
-            inputs = inputs.to(device)
-            labels = labels.to(device)
-
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            l = loss(outputs, labels)
-
-            val_epoch_loss += l.item() * inputs.size(0)
-            val_epoch_acc += accuracy_torch(outputs, labels) * inputs.size(0)
+        val_epoch_loss, val_epoch_acc = evaluate_model_categorization(
+            model, val_loader, loss, device
+        )
 
         if val_epoch_loss < best_loss:
             best_loss = val_epoch_loss
+            best_accuracy = val_epoch_acc
             best_model = deepcopy(model.state_dict())
 
         losses["train"].append(train_epoch_loss / train_size)
-        losses["val"].append(val_epoch_loss / val_size)
+        losses["val"].append(val_epoch_loss)
 
         accuracies["train"].append(train_epoch_acc / train_size)
-        accuracies["val"].append(val_epoch_acc / val_size)
+        accuracies["val"].append(val_epoch_acc)
 
         print(
             f"Train loss: {train_epoch_loss / train_size}, Train Accuracy: {train_epoch_acc / train_size}"
         )
-        print(
-            f"Val. loss: {val_epoch_loss / val_size}, Val. Accuracy: {val_epoch_acc / val_size}"
-        )
+        print(f"Val. loss: {val_epoch_loss}, Val. Accuracy: {val_epoch_acc}")
 
     print("-" * 12)
-    print(f"Best val. loss: {best_loss/val_size}")
+    print(f"Best val. loss: {best_loss}, Accuracy: {best_accuracy}")
 
     model.load_state_dict(best_model)
     return model, losses, accuracies
