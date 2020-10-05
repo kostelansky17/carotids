751f7865fe0455c979d0a9b7e0f4d1c9d31b5979
Martin Kostelansky <kostelansky17@github.com>
2020-10-05 22:50:28 +0200

    move run to cpu only

--- localization_use.py
+++ localization_use.py
@@ -1,21 +1,20 @@
-from torch import load
+from torch import device, load, no_grad
 from torch.utils.data import DataLoader
 from torchvision.transforms import Compose, Normalize, ToTensor
 
 from carotids.localization.frcnn_dataset import FastCarotidDatasetEval
 from carotids.localization.models import create_faster_rcnn
 
+DEVICE = device("cpu")
 TRANSFORMATIONS = Compose(
-    [
-        ToTensor(),
-        Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-    ]
+    [ToTensor(), Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]
 )
 
-PATH_TO_DATA = "FILL_ME"
-PATH_TO_MODEL = "FILL_ME"
+PATH_TO_DATA = "/home/martin/Documents/cartroids/data_samples/localization_samples"
+PATH_TO_MODEL = "/home/martin/Documents/cartroids/models/localization_model.pth"
 
 
+@no_grad()
 def localization_example_use() -> None:
     """Example usage of localization model. Load model from path selected by 
     parameter PATH_TO_MODEL. Evaluates the images in the folder specified by 
@@ -23,13 +22,15 @@ def localization_example_use() -> None:
     most probable carotid on the image with its score.
     """
     model = create_faster_rcnn()
-    model.load_state_dict(load(PATH_TO_MODEL))
+    model.load_state_dict(load(PATH_TO_MODEL, map_location=DEVICE))
+    model.to(DEVICE)
     model.eval()
 
     dataset = FastCarotidDatasetEval(PATH_TO_DATA, TRANSFORMATIONS)
     loader = DataLoader(dataset, batch_size=1, shuffle=False)
 
     for image_tensor, image_name in loader:
+        image_tensor = image_tensor.to(DEVICE)
         outputs = model(image_tensor)
         print(
             "Image {} : Coordinates:{}, Score:{:.2f}%".format(
