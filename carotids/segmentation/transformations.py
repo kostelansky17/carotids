037a9f2cf43205f34d09ed3c3141c4cfdc4d4af0
Martin Kostelansky <mkostelansky@gmail.com>
2021-01-01 21:36:45 +0100

    tr ed

--- carotids/segmentation/transformations.py
+++ carotids/segmentation/transformations.py
@@ -91,65 +91,25 @@ class SegRandomVerticalFlip:
         return img, mask
 
 
-class SegRandomRotation:
-    """Random rotation.
-
-    Represents random rotation which transforms image and segmentation mask.
-    """
-
-    def __init__(self, p: float = 0.5, degrees: list = [90, 180, 270]):
-        """Initializes a random rotation.
-
-        Parameters
-        ----------
-        p : float
-            Probability with which transformation is applied.
-        degrees : list
-            list of degrees to select from
-        """
-        self.p = p
-        self.rotations = [RandomRotation(degree) for degree in degrees]
-
-    def __call__(self, img: Image, mask: Image) -> tuple:
-        """Applies random rotation on an image and a mask.
-
-        Parameters
-        ----------
-        img : Image
-            Image to transform.
-        mask : Image
-            Mask to transform.
-
-        Returns
-        -------
-        tuple
-            Transformed image and mask.
-        """
-        if uniform() <= self.p:
-            degree = randint(len(self.rotations))
-
-            img = self.rotations[degree](img)
-            mask = self.rotations[degree](mask)
-
-        return img, mask
-
-
 class SegCrop:
     """Random crop.
 
-    Represents random vertical crop which transforms image and segmentation mask.
+    Represents random vertical crop which transforms image and segmentation mask. 
     Crops an input image so that whole object is perserved.
     """
 
-    def __init__(self, t: int = 0):
+    def __init__(self, random_t: int = 0, default_t: int = 5):
         """Initializes a random crop.
 
         Parameters
         ----------
-        t : int
+        random_t : int
+            TODO.
+        default_t : int
             TODO.
         """
-        self.t = t
+        self.random_t = random_t
+        self.default_t = default_t
 
     def __call__(self, img: Image, mask: Image) -> tuple:
         """Applies random crop on an image and a mask.
@@ -166,25 +126,26 @@ class SegCrop:
         tuple
             Transformed image and mask.
         """
-        mask_indicies = where((asarray(mask) == [255, 255, 255]).any(axis=2))
+        mask_indicies = where(
+            (asarray(mask) == [255, 255, 255]).any(axis=2)
+        )
 
         x1, y1 = mask_indicies[1].min(), mask_indicies[0].min()
         x2, y2 = mask_indicies[1].max(), mask_indicies[0].max()
 
-        if self.t != 0:
-            t_x1, t_y1, t_x2, t_y2 = (
-                randint(self.t),
-                randint(self.t),
-                randint(self.t),
-                randint(self.t),
-            )
-        else:
-            t_x1, t_y1, t_x2, t_y2 = 0, 0, 0, 0
-
         width, height = img.size
-        x1, y1 = max(x1 - t_x1, 0), max(y1 - t_y1, 0)
-        x2, y2 = min(x2 + t_x2, width), min(y2 + t_y2, height)
-
+        
+        if self.random_t != 0:
+            t = randint(self.random_t)
+        
+            x1, y1 = max(x1 - t, 0), max(y1 - t, 0)
+            x2, y2 = min(x2 + t, width), min(y2 + t, height)
+        else:
+            t = self.default_t
+        
+            x1, y1 = max(x1 - t, 0), max(y1 - t, 0)
+            x2, y2 = min(x2 + t, width), min(y2 + t, height)
+        
         return img.crop((x1, y1, x2, y2)), mask.crop((x1, y1, x2, y2))
 
 
