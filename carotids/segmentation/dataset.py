5e9c582e76e66d09df1819fbf92067659b37ee94
Martin Kostelansky <mkostelansky@gmail.com>
2021-03-25 10:00:29 +0100

    adjust eval ds for labeled data

--- carotids/segmentation/dataset.py
+++ carotids/segmentation/dataset.py
@@ -9,6 +9,43 @@ from carotids.preprocessing import load_img
 from carotids.segmentation.transformations import SegCompose
 
 
+def label_to_mask(
+    label: Tensor, plaque_with_wall: bool = False, encode_to_one_hot: bool = True
+):
+    """Processes label to a Tensor mask. If plaque_with_wall
+    is selected to True, the plaque category is changed
+    to the wall category. If encode_to_one_hot is selected to True,
+    the mask is encoded in a one-hot setting.
+
+    Parameters
+    ----------
+    label : Tensor
+        Index of an item to return.
+    plaque_with_wall : bool
+        If True, the plaque and wall classes are united.
+    encode_to_one_hot : bool
+        If true, the reference is returned encoded in a one-hot setting,
+        if false, the reference is returned with the classes encoded as an
+        integer values.
+
+    Returns
+    -------
+    Tensor
+        Tensor label.
+    """
+    mask = cat((zeros(1, *label.shape[1:]), label))
+    mask = mask.argmax(0)
+
+    if plaque_with_wall:
+        mask[mask == 3] = 1
+
+    if encode_to_one_hot:
+        mask = one_hot(mask).permute(2, 0, 1)
+        mask[0, (label[0, ...] == 1) & (label[1, ...] == 1)] = 1
+
+        return mask
+
+
 class SegmentationDataset(Dataset):
     """Represents a dateset used for segmentation.
 
@@ -74,7 +111,7 @@ class SegmentationDataset(Dataset):
         img, label = self.transformations_custom(img, label)
         img, label = self.transformations_torch(img), self.transformations_torch(label)
 
-        label = self._label_to_mask(label)
+        label = label_to_mask(label, self.plaque_with_wall, self.encode_to_one_hot)
 
         return img, label
 
@@ -88,34 +125,6 @@ class SegmentationDataset(Dataset):
         """
         return len(self.img_files)
 
-    def _label_to_mask(self, label: Tensor):
-        """Processes label to a Tensor mask. If plaque_with_wall
-        is selected to True, the plaque category is changed
-        to the wall category. If encode_to_one_hot is selected to True,
-        the mask is encoded in a one-hot setting.
-
-        Parameters
-        ----------
-        label : Tensor
-            Index of an item to return.
-
-        Returns
-        -------
-        Tensor
-            Tensor label.
-        """
-        mask = cat((zeros(1, *label.shape[1:]), label))
-        mask = mask.argmax(0)
-
-        if self.plaque_with_wall:
-            mask[mask == 3] = 1
-
-        if self.encode_to_one_hot:
-            mask = one_hot(mask).permute(2, 0, 1)
-            mask[0, (label[0, ...] == 1) & (label[1, ...] == 1)] = 1
-    
-        return mask
-
 
 class SegmentationEvaluationDataset(Dataset):
     """Represents a dateset used to create segmentation masks of the images.
@@ -126,7 +135,13 @@ class SegmentationEvaluationDataset(Dataset):
     """
 
     def __init__(
-        self, data_path: str, transformations: Compose, labels_path: str = None
+        self,
+        data_path: str,
+        transformations_torch: Compose,
+        transformations_custom: SegCompose = None,
+        labels_path: str = None,
+        plaque_with_wall: bool = False,
+        encode_to_one_hot: bool = True,
     ) -> None:
         """Initializes an evaluation segmentation dataset.
 
@@ -134,16 +149,29 @@ class SegmentationEvaluationDataset(Dataset):
         ----------
         data_path : str
             Folder with the raw images.
-        transformations : Compose
-            Composition of torch image transformations.
+        transformations_torch : Compose
+            Composition of torch segmentation transformations.
+        transformations_custom : SegCompose
+            Composition of custom segmentation transformations.
         labels_path : str
             Folder with the references.
+        plaque_with_wall : bool
+            If True, the plaque and wall classes are united.
+        encode_to_one_hot : bool
+            If true, the reference is returned encoded in a one-hot setting,
+            if false, the reference is returned with the classes encoded as an
+            integer values.
         """
         self.data_path = data_path
         self.labels_path = labels_path
 
         self.img_files = sorted(listdir(data_path))
-        self.transformations = transformations
+
+        self.transformations_torch = transformations_torch
+        self.transformations_custom = transformations_custom
+
+        self.plaque_with_wall = plaque_with_wall
+        self.encode_to_one_hot = encode_to_one_hot
 
     def __getitem__(self, index: int) -> tuple:
         """Gets item from the dataset at a specified index.
@@ -161,14 +189,26 @@ class SegmentationEvaluationDataset(Dataset):
             as well as an image.
         """
         img = load_img(self.data_path, self.img_files[index])
-        img_torch = self.transformations(img)
 
         if self.labels_path is not None:
             label = load_img(self.labels_path, self.img_files[index])
 
-            return img_torch, img, self.img_files[index], label
+            if self.transformations_custom is not None:
+                img, label = self.transformations_custom(img, label)
 
-        return img_torch, img, self.img_files[index]
+            img_torch = self.transformations_torch(img)
+            label_torch = self.transformations_torch(label)
+
+            label_torch = label_to_mask(
+                label_torch, self.plaque_with_wall, self.encode_to_one_hot
+            )
+
+            return img_torch, img, self.img_files[index], label_torch, label
+
+        else:
+            img_torch = self.transformations_torch(img)
+
+            return img_torch, img, self.img_files[index]
 
     def __len__(self) -> int:
         """Returns a length of the dataset.
