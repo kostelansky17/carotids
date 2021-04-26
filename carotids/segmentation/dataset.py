1c6a871e06de966b1f39498965ea2d721492aedd
Martin Kostelansky <mkostelansky@gmail.com>
2021-04-26 17:37:05 +0200

    crate data module for segmentation

--- carotids/segmentation/dataset.py
+++ carotids/segmentation/dataset.py
@@ -2,11 +2,13 @@ from os import listdir
 
 from torch import cat, int64, Tensor, unsqueeze, zeros
 from torch.nn.functional import one_hot
+from torch.utils.data import DataLoader
 from torch.utils.data.dataset import Dataset
 from torchvision.transforms import Compose
 
 from carotids.preprocessing import load_img
 from carotids.segmentation.transformations import SegCompose
+from carotids.utils import split_dataset, split_dataset_into_dataloaders
 
 
 def label_to_mask(
@@ -44,6 +46,85 @@ def label_to_mask(
         mask[0, (label[0, ...] == 1) & (label[1, ...] == 1)] = 1
 
     return mask
+
+
+class SegmentationDatamodule:
+    """Represent Datamodule used in the segmentation. This class shatters all of
+    the datasets used for training and evaluation - train, validation, and test.
+    """
+
+    def __init__(
+        self,
+        imgs_path: str,
+        labels_path: str,
+        trans_seg_train: SegCompose,
+        trans_seg_val: SegCompose,
+        trans_torch: Compose,
+        batch_size: int = 2,
+        val_split: float = 0.1,
+        test_split: float = 0.1,
+    ) -> None:
+        """Initializes a segmentation datamodule.
+
+        Parameters
+        ----------
+        imgs_path : str
+            Folder with raw images.
+        labels_path : str
+            Folder with references
+        trans_seg_train : SegCompose
+            Composition of custom segmentation transformations for training.
+        trans_seg_val : SegCompose
+            Composition of custom segmentation transformations for evaluation.
+        trans_torch : Compose
+            Composition of torch segmentation transformations.
+        batch_size : int
+            Number of samples in the training samples.
+        val_split : float
+            Precentage of the training data split as the validation set.
+        test_split : float
+            Precentage of the data split as the test set.
+        """
+        dataset = SegmentationDataset(
+            imgs_path,
+            labels_path,
+            trans_seg_train,
+            trans_torch,
+            False,
+            True,
+        )
+
+        dataset_simple = SegmentationDataset(
+            imgs_path,
+            labels_path,
+            trans_seg_val,
+            trans_torch,
+            False,
+            True,
+        )
+
+        train_set, _, _, _ = split_dataset(dataset, test_split)
+        train_set_simple, _, test_set, _ = split_dataset(dataset_simple, test_split)
+
+        self.train_loader, _, _, _ = split_dataset_into_dataloaders(
+            train_set, val_split, batch_size
+        )
+        _, _, self.val_loader, _ = split_dataset_into_dataloaders(
+            train_set_simple, val_split, batch_size
+        )
+        self.test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
+
+    def get_train_loader(self) -> DataLoader:
+        """Returns train DataLoader."""
+        self.train_loader
+
+    def get_val_loader(self) -> DataLoader:
+        """Returns validation DataLoader."""
+        self.val_loader
+
+    def get_test_loader(self) -> DataLoader:
+        """Returns test DataLoader."""
+        self.test_loader
 
 
 class SegmentationDataset(Dataset):
